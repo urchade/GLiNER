1e7f20d22828861a711330d0c5f0e0ab94d8ead0
urchadezaratiana <Riazcass74>
2024-04-22 17:49:04 +0200

    v0.1.11 fix empty texts for batch prediction

--- gliner/modules/base.py
+++ gliner/modules/base.py
@@ -23,15 +23,8 @@ class InstructBase(nn.Module):
 
     def preprocess_spans(self, tokens, ner, classes_to_id):
 
-        # if no tokens
         if len(tokens) == 0:
-            return {
-                "tokens": ["[PAD]"],  # default token
-                "span_idx": torch.LongTensor([[0, 0]]),
-                "span_label": torch.LongTensor([-1]),
-                "seq_length": 1,
-                "entities": [],
-            }
+            tokens = ["[PAD]"]
 
         max_len = self.base_config.max_len
 
