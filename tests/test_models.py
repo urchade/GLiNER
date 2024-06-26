dc01628a5ed0aba33eab18f8ec7eae90d77e76e8
Ihor Stepanov <igor.stepanov2000@gmail.com>
2024-06-26 20:27:13 +0300

    add docstring to the model class

---
+++ tests/test_models.py
@@ -0,0 +1,14 @@
+from gliner import GLiNER
+
+def test_span_model():
+    model = GLiNER.from_pretrained("gliner-community/gliner_small-v2.5")
+
+    text = """
+    Cristiano Ronaldo dos Santos Aveiro (Portuguese pronunciation: [kɾiʃˈtjɐnu ʁɔˈnaldu]; born 5 February 1985) is a Portuguese professional footballer who plays as a forward for and captains both Saudi Pro League club Al Nassr and the Portugal national team. Widely regarded as one of the greatest players of all time, Ronaldo has won five Ballon d'Or awards,[note 3] a record three UEFA Men's Player of the Year Awards, and four European Golden Shoes, the most by a European player. He has won 33 trophies in his career, including seven league titles, five UEFA Champions Leagues, the UEFA European Championship and the UEFA Nations League. Ronaldo holds the records for most appearances (183), goals (140) and assists (42) in the Champions League, goals in the European Championship (14), international goals (128) and international appearances (205). He is one of the few players to have made over 1,200 professional career appearances, the most by an outfield player, and has scored over 850 official senior career goals for club and country, making him the top goalscorer of all time.
+    """
+
+    labels = ["person", "award", "date", "competitions", "teams"]
+
+    entities = model.predict_entities(text, labels)
+
+    assert len(entities)>0
