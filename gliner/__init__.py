bf9792de30f48bd9610d08a68fe0632ec86befe5
urchade <urchadcassim@outlook.fr>
2025-01-02 21:43:44 +0400

    add v

--- gliner/__init__.py
+++ gliner/__init__.py
@@ -1,9 +1,9 @@
-__version__ = "0.2.13"
+__version__ = "0.2.14"
 
 from .model import GLiNER
 from .config import GLiNERConfig
-from .multitask import (GLiNERClassifier, GLiNERQuestionAnswerer, GLiNEROpenExtractor,
-                                GLiNERRelationExtractor, GLiNERSummarizer, GLiNERSquadEvaluator,
-                                    GLiNERDocREDEvaluator)
+# from .multitask import (GLiNERClassifier, GLiNERQuestionAnswerer, GLiNEROpenExtractor,
+#                                 GLiNERRelationExtractor, GLiNERSummarizer, GLiNERSquadEvaluator,
+#                                     GLiNERDocREDEvaluator)
 
 __all__ = ["GLiNER"]
