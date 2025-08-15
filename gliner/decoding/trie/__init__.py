b88d9d2cab0de5827928a647df88cc816104039b
Ingvar <igor.stepanov2000@gmail.com>
2025-08-15 17:15:27 +0300

    use python labels trie if cython is unavailable

--- gliner/decoding/trie/__init__.py
+++ gliner/decoding/trie/__init__.py
@@ -3,7 +3,9 @@ from ...utils import is_module_available
 if is_module_available("pyximport"):
     import pyximport # type: ignore
     pyximport.install() # type: ignore
-    from gliner.decoding.trie.labels_trie import LabelsTrie
-
+    try:
+        from gliner.decoding.trie.labels_trie import LabelsTrie
+    except:
+        from .python_labels_trie import LabelsTrie
 else:
     from .python_labels_trie import LabelsTrie
